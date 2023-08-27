db109cebe7e8f69591c27759a055e020ccfbae1d
rhakagesa <rhakagesa.gmail.com>
2023-08-27 17:00:16 +0700

    Penambahan footer

--- script/script.js
+++ script/script.js
@@ -1,3 +1,3 @@
-let hiname = document.querySelector("#hiname");
-
+const hiname = document.querySelector("#hiname");
+// const inputName = prompt("Masukkan nama untuk menyamapamu");
 hiname.innerHTML = "Rhaka";
